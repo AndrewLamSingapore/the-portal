2136d10ae143fdcbdc5117852bf6022401dc49f4
Andrew Lam <andrew3657@hotmail.com>
2026-08-27 19:57:02 -0700

    Preserve the graph no-cache compatibility contract

--- api/graph.js
+++ api/graph.js
@@ -4,7 +4,7 @@ import { buildKnowledgeGraph } from '../lib/knowledge.js';
 
 export default async function handler(req, res) {
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.setHeader('Cache-Control', 'no-store');
+  res.setHeader('Cache-Control','no-store');
   if (req.method !== 'GET') {
     res.setHeader('Allow', 'GET');
     return res.status(405).json({ error: 'Method not allowed' });
